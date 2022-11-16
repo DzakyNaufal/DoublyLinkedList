833c7710001864028a87de7c71b76719756c55bf
DzakyNaufal <dzakynmulna@gmail.com>
2022-11-16 17:11:28 +0700

    menambahkan check ketika listnya kosong

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -17,6 +17,43 @@ namespace DoublyLinkedList
         public Node next;/*Points to the succeeding node*/
         public Node prev;/*Points to the preceeding node*/
     }
+
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()/*Adds a new node*/
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
