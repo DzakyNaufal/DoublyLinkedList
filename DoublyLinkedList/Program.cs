b6a66c4b39b0c0c0c9c8daee4e80867f42ad4a81
DzakyNaufal <dzakynmulna@gmail.com>
2022-11-16 16:48:44 +0700

    membuat class node dan menambahkan function public next dan prev untuk node

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
+    class Node
+    {
+        /*Node class represents the node of doubly linked list.
+         * It consist of the information part and links to
+         * Its succeeding and preceeding nodes
+         * In terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*Points to the succeeding node*/
+        public Node prev;/*Points to the preceeding node*/
+    }
     internal class Program
     {
         static void Main(string[] args)
